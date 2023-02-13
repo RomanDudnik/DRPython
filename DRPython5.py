540c5f922e1972b2d50a085cf49f4af1367881d3
«Roman <research777@mail.ru>
2023-02-13 16:04:19 +0300

    Ex28.v2

--- DRPython5.py
+++ DRPython5.py
@@ -28,16 +28,38 @@ A = 2; B = 3 -> 8
 2 2
 4
 '''
+# Решение по условию задачи:
+    
+def sumNum(a, b):
+    if a == 0 or b == 0:
+        if a > b:
+            return a
+        return b
+    elif (b == 1):
+        return a + 1
+    return sumNum(a+1, b-1)
+        
+num_1 = int(input('Input first Positive! number: '))  
+num_2 = int(input('Input second Positive! number: '))
+print(f'{num_1} + {num_2} = {sumNum(num_1, num_2)}')
+
+# Решение задачи для любых целых чисел:
+
 # def sumNum(a, b):
 #     if a == 0 or b == 0:
 #         if a > b:
 #             return a
 #         return b
+    
+#     elif b < 0:
+#         if (b == -1):
+#                 return a - 1
+#         return sumNum(a - 1, b + 1)    
+        
 #     elif (b == 1):
 #         return a + 1
-#     return sumNum(a+1, b-1)
+#     return sumNum(a + 1, b - 1)
         
-# num_1 = int(input('Input first Positive! number: '))  
-# num_2 = int(input('Input second Positive! number: '))
-# print(sumNum(num_1, num_2))
-
+# num_1 = int(input('Input first number: '))  
+# num_2 = int(input('Input second number: '))
+# print(f'{num_1} + {num_2} = {sumNum(num_1, num_2)}')
